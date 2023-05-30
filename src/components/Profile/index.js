78e3c9020dfc2b51ef45080a7b30e8ca4c25b7e3
GirardiGG <gustavo_awd@hotmail.com>
2023-05-30 10:28:28 -0300

    initial structure gatsby

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Matt Foo</h1>
+        <h2>Programador Backend</h2>
+        <p>Dev backend</p>
+    </div>
+)
+
+export default Profile
